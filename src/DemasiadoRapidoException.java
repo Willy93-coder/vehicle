ee958e3d483c869c05af6d4d1df03ac4f61bc0c4
Willy <guillecg13@gmail.com>
2023-01-30 18:16:55 +0100

    Añadido java doc

--- src/DemasiadoRapidoException.java
+++ src/DemasiadoRapidoException.java
@@ -1,3 +1,9 @@
+/**
+ * Clase DemasiadoRapidoException
+ * Clase que gestiona la excepción de demasiadoRapidoException y hereda de Exception
+ * @author Willy
+ * @version 1.0.0
+ * */
 public class DemasiadoRapidoException extends Exception{
     public DemasiadoRapidoException(String message) {
         super(message);
